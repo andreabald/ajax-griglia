68dfebd3f3d70b852bce9b092fb61c9c98802357
Andrea Baldocchi <andrea.baldocchi21@gmail.com>
2019-05-27 13:36:52 +0200

    FINE

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// Creare una griglia 6x6, ad ogni click su un riquadro parte una richiesta AJAX che prende un numero random da 1 a 9 (utilizzare l'API https://www.boolean.careers/api/random/int).
+// Se è <= 5 il quadrato diventa giallo, se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+
 $(document).ready(function(){
   var quadratino = $('#template_quadratini').html();
   var template_quadratino_function = Handlebars.compile(quadratino);
